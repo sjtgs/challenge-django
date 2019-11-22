25658d2089143117568d83171c98bbfbb621fd5e
sjtg <chipilej@gmail.com>
2019-11-22 05:04:08 +0200

    Created Custom User model

--- challenge_django/accounts/models.py
+++ challenge_django/accounts/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
+from django.db.models.signal import post_save
+from django.dispatch import receiver
+from datetime import datetime
+from django.utils import timezone
+
 from django.db import models
 
-# Create your models here.
+
+#Create user profile 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.user.username
+
+    
